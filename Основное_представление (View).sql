9807ba6ba28a4195745f6784a9fc9e9120641c54
Slipslon <slip_mail@mail.ru>
2024-06-30 19:05:41 +0300

    Update Основное_представление (View).sql

--- Основное_представление (View).sql
+++ Основное_представление (View).sql
@@ -1,42 +1,42 @@
 
----- Îñíîâíîå ïðåäñòàâëåíèå äëÿ çàãðóçêè â PowerBI
+---- Основное представление для загрузки в PowerBI
 
 ---- USE TNKFU
 
----- ALTER VIEW TNKFU AS  -- äëÿ èçìåíåíèÿ
+---- ALTER VIEW TNKFU AS  -- для изменения
 
------------- Áëîê CTE çàïðîñîâ ----------------------
+------------ Блок CTE запросов ----------------------
 
 WITH AmountCTE as /*
 
-			Áëîê Îñíîâíîé èíôîðìàöèè (
+			Блок Основной информации (
 
 */ (
 	SELECT 
-	s.IDSmetPos AS [ID] -- ID ñìåòíîé ïîçèöèè, äëÿ ñîðòèðîâêè
-	,s.NameSmetPos AS [Ñìåò.ï.] -- Ñìåòíàÿ ïîçèöèÿ
-	,w.NameWork AS [Íàèìåíîâàíèå ðàáîò]
-	,w.Unit AS [Åä.èçì.]
-	,ads.Amount AS [Êîë-âî ñìåòà]
-	,ads.Material_price AS [Öåíà ìàò.]
-	,ads.Work_price AS [Öåíà ðàá.]
-	,ISNULL(ads.Material_price,0)+ISNULL(ads.Work_price,0) AS [Åä öåíà]
-	,n.Íàêîïèòåëüíàÿ AS [Íàêîïèòåëüíàÿ]
-	,ISNULL(ads.Amount,0)-ISNULL(n.Íàêîïèòåëüíàÿ,0) AS [Îñòàòîê]
-	,cor.[Ñóììà COR] AS [Ñóììà COR]
-	,ISNULL(cor.[Ñóììà COR],0)+ISNULL(ads.Amount,0) AS [Êîë-âî ñ CORàìè]
-	,idoc.[Èòîã ÈÄ] AS [Èòîã ÈÄ]
-	,ISNULL(idoc.[Èòîã ÈÄ],0) - ISNULL(n.Íàêîïèòåëüíàÿ,0) AS [Ðàçíèöà]
-	,s.IDInd1 AS [Èíäèêàòîð 1]
-	,s.IDInd2 AS [Èíäèêàòîð 2]
-	,s.IDInd3 AS [Èíäèêàòîð 3]
-	,s.IDInd7 AS [Èíäèêàòîð 7]
-	,MaE.Code AS [ID Ìàòåðèàëà]
+	s.IDSmetPos AS [ID] -- ID сметной позиции, для сортировки
+	,s.NameSmetPos AS [Смет.п.] -- Сметная позиция
+	,w.NameWork AS [Наименование работ]
+	,w.Unit AS [Ед.изм.]
+	,ads.Amount AS [Кол-во смета]
+	,ads.Material_price AS [Цена мат.]
+	,ads.Work_price AS [Цена раб.]
+	,ISNULL(ads.Material_price,0)+ISNULL(ads.Work_price,0) AS [Ед цена]
+	,n.Накопительная AS [Накопительная]
+	,ISNULL(ads.Amount,0)-ISNULL(n.Накопительная,0) AS [Остаток]
+	,cor.[Сумма COR] AS [Сумма COR]
+	,ISNULL(cor.[Сумма COR],0)+ISNULL(ads.Amount,0) AS [Кол-во с CORами]
+	,idoc.[Итог ИД] AS [Итог ИД]
+	,ISNULL(idoc.[Итог ИД],0) - ISNULL(n.Накопительная,0) AS [Разница]
+	,s.IDInd1 AS [Индикатор 1]
+	,s.IDInd2 AS [Индикатор 2]
+	,s.IDInd3 AS [Индикатор 3]
+	,s.IDInd7 AS [Индикатор 7]
+	,MaE.Code AS [ID Материала]
 	,s.MAF
-	,m.Status AS [Ñòàòóñ MAF]
-	,ar.Çàêóïêà
-	,ar.Îòâåòñòâåííûé
-	,ar.Çàÿâêè AS [Çàÿâêè]
+	,m.Status AS [Статус MAF]
+	,ar.Закупка
+	,ar.Ответственный
+	,ar.Заявки AS [Заявки]
 
 	FROM Smeta s
 	
@@ -45,8 +45,8 @@ WITH AmountCTE as /*
 			
 			LEFT JOIN (/*
 	
-					Îáúåì îòíîñèòåëüíî êàêîãî-òî êîíêðåòíîãî ÄC
-					SUM äëÿ íåñêîëüêèõ ÄÑ (êîãäà ðàçäåëèëè íà ýòàïû)
+					Объем относительно какого-то конкретного ДC
+					SUM для нескольких ДС (когда разделили на этапы)
 
 			*/
 				SELECT
@@ -61,12 +61,12 @@ WITH AmountCTE as /*
 				
 			LEFT JOIN (/*
 	
-					Íàêîïèòåëüíàÿ ïî âñåìó îáúåìó ÊÑ. Ñóììèðóþòñÿ òîëüêî îáúåìû, òî åñòü íå ïðèâÿçàíî ê ÄÑ
+					Накопительная по всему объему КС. Суммируются только объемы, то есть не привязано к ДС
 	
 			*/
 				SELECT
 				IDSmetPos,
-				SUM(Amount) [Íàêîïèòåëüíàÿ]
+				SUM(Amount) [Накопительная]
 				FROM Amount_KS
 				GROUP BY IDSmetPos
 				) n
@@ -75,12 +75,12 @@ WITH AmountCTE as /*
 			
 			LEFT JOIN (/*
 	
-					Íàêîïèòåëüíàÿ ïî âñåìó îáúåìó COR
+					Накопительная по всему объему COR
 	
 			*/
 				SELECT
 				IDSmetPos,
-				SUM(Amount) [Ñóììà COR]
+				SUM(Amount) [Сумма COR]
 				FROM Amount_COR
 				GROUP BY IDSmetPos
 				) cor
@@ -88,12 +88,12 @@ WITH AmountCTE as /*
 			
 			LEFT JOIN (/*
 	
-					Íàêîïèòåëüíàÿ ïî âñåìó îáúåìó ÈÄ. Ñóììèðóþòñÿ òîëüêî îáúåìû, òî åñòü íå ïðèâÿçàíî ê ÄÑ.
+					Накопительная по всему объему ИД. Суммируются только объемы, то есть не привязано к ДС.
 	
 			*/
 				SELECT
 				IDSmetPos
-				,SUM(Amount) AS [Èòîã ÈÄ]
+				,SUM(Amount) AS [Итог ИД]
 				FROM Amount_IDoc
 				WHERE [ID_IDoc] IN (1)
 				GROUP BY IDSmetPos
@@ -107,14 +107,14 @@ WITH AmountCTE as /*
 			
 			LEFT JOIN (/*
 	
-					Íàêîïèòåëüíàÿ ïî âñåìó îáúåìó Çàÿâîê. Ñóììèðóþòñÿ òîëüêî îáúåìû, òî åñòü íå ïðèâÿçàíî ê ÄÑ
+					Накопительная по всему объему Заявок. Суммируются только объемы, то есть не привязано к ДС
 	
 			*/
 				SELECT
 				a_r.IDSmetPos
-				,STRING_AGG(a_r.IDRequest,', ') [Çàÿâêè]
-				,SUM(a_r.Amount) [Çàêóïêà]
-				,STRING_AGG(r.Responsible,', ') WITHIN GROUP ( ORDER BY r.Responsible ASC) [Îòâåòñòâåííûé]
+				,STRING_AGG(a_r.IDRequest,', ') [Заявки]
+				,SUM(a_r.Amount) [Закупка]
+				,STRING_AGG(r.Responsible,', ') WITHIN GROUP ( ORDER BY r.Responsible ASC) [Ответственный]
 				FROM Amount_Request a_r
 				
 				LEFT JOIN (
@@ -135,8 +135,8 @@ WITH AmountCTE as /*
 	,RequestCTE AS ( -- 
 				SELECT 
 					IDSmetPos,
-					[Èâàíîâ1],
-					[Èâàíîâ2]
+					[Иванов1],
+					[Иванов2]
 				FROM (
 					SELECT 
 						IDSmetPos,
@@ -146,42 +146,42 @@ WITH AmountCTE as /*
 				) AS SourceTable
 				PIVOT (
 					SUM(Amount)  
-					FOR Responsible IN ([Èâàíîâ1],[Èâàíîâ2])
+					FOR Responsible IN ([Иванов1],[Иванов2])
 				) AS PivotTable
 	)
 	
 	
 
 
-----------------------------Îñíîâíîé áëîê çàïðîñà ïî âûøåîïèñàííûì CTE
+----------------------------Основной блок запроса по вышеописанным CTE
 SELECT 
-ROW_NUMBER() OVER(ORDER BY Acte.ID ASC) AS R#   -- Íîìåðà ñòðîê, äëÿ ñîðòèðîâêè â Power BI, òàê êàê òàì íå ïîääåðæèâàåòñÿ ñîðòèðîâêà ïî hierarchyid
+ROW_NUMBER() OVER(ORDER BY Acte.ID ASC) AS R#   -- Номера строк, для сортировки в Power BI, так как там не поддерживается сортировка по hierarchyid
 ,Acte.ID.ToString() AS ID
-,Acte.[Ñìåò.ï.]
-,Acte.[Íàèìåíîâàíèå ðàáîò]
-,Acte.[Êîë-âî ñìåòà]
-,Acte.[Öåíà ìàò.]
-,Acte.[Öåíà ðàá.]
-,Acte.[Åä öåíà]
-,Acte.[Êîë-âî ñìåòà]*Acte.[Åä öåíà] AS [Ñòîèìîñòü ñìåòà]
-,Acte.[Íàêîïèòåëüíàÿ]
-,Acte.[Íàêîïèòåëüíàÿ]*Acte.[Åä öåíà] AS [Ñòîèìîñòü Íàê.]
-,Acte.[Îñòàòîê]
-,Acte.[Îñòàòîê]*Acte.[Åä öåíà] AS [Ñòîèìîñòü Îñò.]
-,Acte.[Ñóììà COR]
-,Acte.[Êîë-âî ñ CORàìè]
-,Acte.[Êîë-âî ñ CORàìè]*Acte.[Åä öåíà] AS [Ñòîèìîñòü c CORàìè]
-,Acte.[Èíäèêàòîð 1]
-,Acte.[Èíäèêàòîð 2]
-,Acte.[Èíäèêàòîð 3]
-,Acte.[Èíäèêàòîð 7]
+,Acte.[Смет.п.]
+,Acte.[Наименование работ]
+,Acte.[Кол-во смета]
+,Acte.[Цена мат.]
+,Acte.[Цена раб.]
+,Acte.[Ед цена]
+,Acte.[Кол-во смета]*Acte.[Ед цена] AS [Стоимость смета]
+,Acte.[Накопительная]
+,Acte.[Накопительная]*Acte.[Ед цена] AS [Стоимость Нак.]
+,Acte.[Остаток]
+,Acte.[Остаток]*Acte.[Ед цена] AS [Стоимость Ост.]
+,Acte.[Сумма COR]
+,Acte.[Кол-во с CORами]
+,Acte.[Кол-во с CORами]*Acte.[Ед цена] AS [Стоимость c CORами]
+,Acte.[Индикатор 1]
+,Acte.[Индикатор 2]
+,Acte.[Индикатор 3]
+,Acte.[Индикатор 7]
 ,Acte.MAF
-,Acte.[Ñòàòóñ MAF]
-,Acte.Çàêóïêà
-,Acte.Îòâåòñòâåííûé
-,Acte.Çàÿâêè
-,Reqcte.[Èâàíîâ1]
-,Reqcte.[Èâàíîâ2]
+,Acte.[Статус MAF]
+,Acte.Закупка
+,Acte.Ответственный
+,Acte.Заявки
+,Reqcte.[Иванов1]
+,Reqcte.[Иванов2]
 
 
 FROM AmountCTE Acte
